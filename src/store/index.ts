d72c58179bfa0d4b3618112c508907c169e28353
Cláudio Pedro Mindouro <claudiopedrom@gmail.com>
2023-07-31 21:28:14 +0200

    feat: actions and typescript support

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { useSelector, TypedUseSelectorHook } from "react-redux";
 
 // First state slice
 // We can have multiple slices, each with their own reducer
 const todoSlice = createSlice({
   name: "todo",
   initialState: ["Todo 1", "Todo 2"],
-  reducers: {},
+
+  // Actions that the interface can call
+  reducers: {
+    add: (state, action) => {
+      state.push(action.payload.newTodo);
+    },
+  },
 });
 
 export const store = configureStore({
@@ -13,3 +20,10 @@ export const store = configureStore({
     todo: todoSlice.reducer,
   },
 });
+
+// Export actions to be used by the interface
+export const { add } = todoSlice.actions;
+
+// Add TypeScript support
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
